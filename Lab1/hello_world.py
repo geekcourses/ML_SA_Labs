a637675ff08a5219c9aba0bd430db067c8593f98
geekcourses <geekcourses@gmail.com>
2022-07-07 21:07:40 +0300

    Lab8: 07.07.2022 DONE: https://geekcourses.github.io/ML_SA-Slides/pages/themes/Python_For_ML_1_1/Python_For_ML_1_1.html#/

--- Lab1/hello_world.py
+++
@@ -1,7 +0,0 @@
-user_age = 12
-user_country = 'UK'
-
-if( user_age>=18 or user_country=='UK' ):
-	print('Welcome')
-
-
---
+++ Lab8/basic_python.py
@@ -0,0 +1,19 @@
+# ------------------------------ numerical types ----------------------------- #
+# integer
+print(+42)
+print(42)
+print(-42)
+
+print(1_000_000)
+print(1000000)
+
+# floating point (реални числа)
+print(3.14)
+print(0.1 + 0.2)
+
+
+# division
+print(5/2)
+print(5/2)  # делене
+print(5//2)  # целочислено делена
+
