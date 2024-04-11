32f5b9897b191745a494ccf6aa01cf5a938d1153
geekcourses <geekcourses@gmail.com>
2024-04-11 21:04:00 +0300

    Lab11: 11.04.2024 DONE: till https://geekcourses.github.io/ML_SA-Slides/pages/themes/Python_intro_basic_data_types/Python_intro_basic_data_types.html#/18

---
+++ lab11/data_types.py
@@ -0,0 +1,75 @@
+
+# ----------------------------- String Data Type ----------------------------- #
+# print( 'this is a string' )
+# print( "this is a string" )
+# print( """Line1
+#             Line2
+#             Line3
+#       """)
+
+
+
+# ### concatenation
+# print( 2+2 ) # addition
+# print( '2'+'2' ) # concatenation
+
+# ### repetion
+# print( 'a' * 10 )
+# print( 10 * 'a' )
+
+### string methods
+# user_name = 'ada'
+# user_name = user_name.capitalize()
+# print(user_name)
+
+# RAM:
+#               :0x123: 'ada'
+#      user_name:0x345: 'Ada'
+
+
+# user_name = 'Ada Byron'
+# print( user_name.find('A'))
+
+# --------------------------------- Variables -------------------------------- #
+# x = 1
+# user_first_name = 'Ada'
+
+# _ = 1111
+# print( _ )
+
+# RAM:
+#     _:0x123: 1111
+
+# usernam11= 1
+# print(usernam11)
+
+
+# user = 'Ada'
+# print(User)
+
+# име = 'Ада'
+# print(име)
+
+# userFirstName = 'Ada'
+# print(userFirstName)
+
+# x = 1
+# print( id(x) )
+
+
+# a = 1
+# b = a
+# c = a
+
+# print( a, b, c )
+
+# a  = 'hi'
+# print( a, b, c )
+
+
+# user_name = input('Enter name: ')
+# # print('Hello ' + user_name + '!')
+# print(f'Hello {user_name} !')
+# print(f'2+2=  }')
+
+
